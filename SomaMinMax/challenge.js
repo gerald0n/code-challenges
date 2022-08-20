ebf12bbffaf7997a8a9403159c862cf2195eb0e4
gerald0n <contato.gneto@gmail.com>
2022-08-20 19:38:29 -0300

    excluido variavel resultOfArrayNumbersSum do escopo local e declarado como parâmetro default da função

--- SomaMinMax/challenge.js
+++ SomaMinMax/challenge.js
@@ -1,9 +1,8 @@
 const arrayNumbers = [1, 2, 3, 4, 5]
 
-const minMaxSum = arr => {
-    let resultOfArrayNumbersSum = []
+const minMaxSum = (arr, resultOfArrayNumbersSum = []) => {
     for (let number of arr) resultOfArrayNumbersSum.push(arr.filter(value => value !== number).reduce((acumulator, currentValue) => acumulator + currentValue))
-    return [`min: ${Math.min(...resultOfArrayNumbersSum)}`,`max: ${Math.max(...resultOfArrayNumbersSum)}`]
+    return [`min: ${Math.min(...resultOfArrayNumbersSum)}`, `max: ${Math.max(...resultOfArrayNumbersSum)}`]
 }
 
 console.log(minMaxSum(arrayNumbers)) // return ['min: 10', 'max: 14']
