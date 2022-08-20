8466229d64acc0804eca0c67262ea9d105732ae5
gerald0n <contato.gneto@gmail.com>
2022-08-20 19:29:38 -0300

    encurtando ao máximo o código, deixando-o mais performático (ou não ksksks)

--- SomaMinMax/challenge.js
+++ SomaMinMax/challenge.js
@@ -2,10 +2,7 @@ const arrayNumbers = [1, 2, 3, 4, 5]
 
 const minMaxSum = arr => {
     let resultOfArrayNumbersSum = []
-
-    for (let number of arr) {
-        resultOfArrayNumbersSum.push(arr.filter(value => value !== number).reduce((acumulator, currentValue) => acumulator + currentValue))
-    }
+    for (let number of arr) resultOfArrayNumbersSum.push(arr.filter(value => value !== number).reduce((acumulator, currentValue) => acumulator + currentValue))
     return [`min: ${Math.min(...resultOfArrayNumbersSum)}`,`max: ${Math.max(...resultOfArrayNumbersSum)}`]
 }
 
